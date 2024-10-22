4655dae6e7676fd5a5bb47d6df926f208e1f8385
Wesley de Oliveira <wesley.oliveirabd@gmail.com>
2024-10-22 11:56:48 -0300

    criado o arquivo de configuração e conexão como banco de dados e incluido o include da conexão na pagina index.php

---
+++ db/config.php
@@ -0,0 +1,7 @@
+<?php
+    //constantes de configuração da conexão com o BD
+    define("SERVIDOR", "localhost");
+    define("USUARIO", "root");
+    define("SENHA", "");
+    define("BANCO", "sisgarantias");
+?>
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php 
+    include("db/conexao.php");
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
